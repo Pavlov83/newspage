b0c4a8a6de822319ed236761dee0ab30f4702a8c
Pavel Pavlov <resolversoft@gmail.com>
2021-01-13 16:43:32 +0200

    Updates

---
+++ precache-manifest.f768bb7c9553c143c7ea10e883e072ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e89a1a4e32c7ca29242de0b30542eb",
+    "url": "/newspage/index.html"
+  },
+  {
+    "revision": "28aeb88b874546dbd640",
+    "url": "/newspage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58fd2e24bd69d257af8d",
+    "url": "/newspage/static/js/2.38cebbf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newspage/static/js/2.38cebbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28aeb88b874546dbd640",
+    "url": "/newspage/static/js/main.e8f31285.chunk.js"
+  },
+  {
+    "revision": "57c21e18c22f12a4514d",
+    "url": "/newspage/static/js/runtime-main.a4dcb5ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newspage/static/media/logo.5d5d9eef.svg"
+  }
+]);
